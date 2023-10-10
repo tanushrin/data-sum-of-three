fe26dc51da8a2c44e4c0c11fe0ce6113a1ddba6d
Tanushri Nayak <tanushri0310003@gmail.com>
2023-10-10 10:27:21 +0200

    Solve first Python exercise ff the day

--- sum_of_three.py
+++ sum_of_three.py
@@ -4,4 +4,5 @@ def sum3(first_number, second_number, third_number):
     """
     Takes three numbers and returns the sum of those numbers
     """
-    pass  # YOUR CODE HERE
+    return first_number + second_number + third_number
+    #pass  # YOUR CODE HERE
